c7c0fd1b827380a5b6f70bae2be92fb3eb6af81f
pallavi.dhepe <Pallavi.Dhepe@tylertech.com>
2019-01-24 10:35:53 +0530

    Samples of gooogle cloud nlp api is added in solution

---
+++ ConsoleAppNLPVADERSampleNetCore/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Tweetinvi;
+using Tweetinvi.Events;
+using Tweetinvi.Models;
+using VaderSharp;
+
+namespace ConsoleAppNLPVADERSampleNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccessTwitterAccount();
+        }
+        private static void AccessTwitterAccount()
+        {
+            string consumerapikey = "vBxKtpyhRecA4H2dktO1Q3G31";
+            string apisecret = "B8TcrqMTPyfnUSoHkVvFfYQ85p3VsZwccw4Z9i7Axl9fo85Iqb";
+            string accesstoken = "186847510-lsaTaQ8V2z5L7qaXioki0L4dJhhZhzpNljM6QxTm";
+            string accesstokensecret = "EFwSevSFzdB34dzcyK7FHpl8oebGbfw3jSWH0wis9l830";
+
+            Auth.SetUserCredentials(consumerapikey, apisecret, accesstoken, accesstokensecret);
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack("mySampleCompany");
+            stream.AddTweetLanguageFilter(LanguageFilter.English);
+            stream.MatchingTweetReceived += OnMatchedTweet_AnalyseUsing_VADER;
+            stream.StartStreamMatchingAllConditions();
+
+            Console.ReadLine();
+        }
+
+        private static void OnMatchedTweet_AnalyseUsing_VADER(object sender, MatchedTweetReceivedEventArgs args)
+        {
+            Console.WriteLine("**********************************New match tweet received*********************");
+            var sanitized = sanitize(args.Tweet.FullText);
+            SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
+
+            var results = analyzer.PolarityScores(sanitized);
+
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine($"tweet :- {args.Tweet}");
+            Console.WriteLine("Positive score: " + results.Positive);
+            Console.WriteLine("Negative score: " + results.Negative);
+            Console.WriteLine("Neutral score: " + results.Neutral);
+            Console.WriteLine("Compound score: " + results.Compound);
+
+           
+          
+            Console.WriteLine("***********************************************************************");
+        }
+
+        private static string sanitize(string raw)
+        {
+            return raw;
+            //return Regex.Replace(raw, @"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ").ToString();
+        }
+    }
+}
---
+++ ConsoleAppUsingGoogleCloudAPI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Google.Cloud.Language.V1;
+using Tweetinvi;
+using Tweetinvi.Events;
+using Tweetinvi.Models;
+
+namespace NaturalLanguageApiDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AccessTwitterAccount();
+        }
+        private static void AccessTwitterAccount()
+        {
+            string consumerapikey = "vBxKtpyhRecA4H2dktO1Q3G31";
+            string apisecret = "B8TcrqMTPyfnUSoHkVvFfYQ85p3VsZwccw4Z9i7Axl9fo85Iqb";
+            string accesstoken = "186847510-lsaTaQ8V2z5L7qaXioki0L4dJhhZhzpNljM6QxTm";
+            string accesstokensecret = "EFwSevSFzdB34dzcyK7FHpl8oebGbfw3jSWH0wis9l830";
+
+            Auth.SetUserCredentials(consumerapikey, apisecret, accesstoken, accesstokensecret);
+            var stream = Stream.CreateFilteredStream();
+            stream.AddTrack("mySampleCompany");
+            stream.AddTweetLanguageFilter(LanguageFilter.English);
+            stream.MatchingTweetReceived += OnMatchedTweet_AnalyseUsing_GoogleCloudNLPAPI;
+            stream.StartStreamMatchingAllConditions();
+
+            Console.ReadLine();
+        }
+        private static void OnMatchedTweet_AnalyseUsing_GoogleCloudNLPAPI(object sender, MatchedTweetReceivedEventArgs args)
+        {
+            Console.WriteLine("**********************************New match tweet received*********************");
+            var sanitized = args.Tweet.FullText;
+            var client = LanguageServiceClient.Create();
+            var response = client.AnalyzeSentiment(Document.FromPlainText(sanitized));
+            var sentiment = response.DocumentSentiment;
+            Console.WriteLine($"tweet :- {args.Tweet}");
+            Console.WriteLine($"Score: {sentiment.Score}");
+            Console.WriteLine($"Magnitude: {sentiment.Magnitude}");
+
+            Console.WriteLine("---------------------------------------");
+
+
+
+            Console.WriteLine("***********************************************************************");
+        }
+
+    }
+}
